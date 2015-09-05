ed99752fbe631d714a59ce8e15b3c5ea87aab07a
Jie Gao <jg3526@users.noreply.github.com>
2015-09-05 11:33:03 -0400

    installed twbs:bootstrap for front-end, and used that to fill in the index page html

--- MeteorMyTodos.js
+++ MeteorMyTodos.js
@@ -2,12 +2,14 @@ if (Meteor.isClient) {
   // counter starts at 0
   Session.setDefault('counter', 0);
 
+  // set variables that could be used inside the html template
   Template.hello.helpers({
     counter: function () {
       return Session.get('counter');
     }
   });
 
+  // set event handler
   Template.hello.events({
     'click button': function () {
       // increment the counter when button is clicked
