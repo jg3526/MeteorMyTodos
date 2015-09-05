70150b2629bc394e2fdbdcf64d8e482bb0f7eb03
Jie Gao <jg3526@users.noreply.github.com>
2015-09-05 14:50:46 -0400

    remove insecure for security, especially necessary for production

--- MeteorMyTodos.js
+++ MeteorMyTodos.js
@@ -10,22 +10,17 @@ if (Meteor.isClient) {
   Template.main.events({
     "submit .new-todo": function(event) {
       var text = event.target.text.value;
-      Todos.insert({
-        text: text,
-        createdAt: new Date(),
-        userId: Meteor.userId(),
-        username: Meteor.user().username
-      });
+      Meteor.call('addTodo', text);
       // clear form
       event.target.text.value = '';
       return false;
     },
     "click .toggle-check": function(event) {
-      Todos.update(this._id, {$set:{checked: !this.checked }});
+      Meteor.call('setChecked', this._id, !this.checked);
     },
     "click .delete-todo": function(event) {
       if (confirm("Are you sure?")){
-        Todos.remove(this._id);
+        Meteor.call('deleteTodo', this._id);
       }
     }
   });
@@ -35,6 +30,28 @@ if (Meteor.isClient) {
   });
     
 }
+
+// for security
+Meteor.methods({
+  addTodo: function(text) {
+    if (!Meteor.userId()){
+      throw new Meteor.Error('not authorized.');
+    }
+    Todos.insert({
+      text: text,
+      createdAt: new Date(),
+      userId: Meteor.userId(),
+      username: Meteor.user().username
+    });
+  },
+  deleteTodo: function(todoId){
+    Todos.remove(todoId);
+  },
+  setChecked: function(todoId, setChecked) {
+    Todos.update(todoId, {$set:{checked: setChecked}});
+  }
+});
+
 
 if (Meteor.isServer) {
   
