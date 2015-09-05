3237758183250257f09f64e2d1768d8f4f3fe2f5
Jie Gao <jg3526@users.noreply.github.com>
2015-09-05 12:03:00 -0400

    showing todos from db

--- MeteorMyTodos.js
+++ MeteorMyTodos.js
@@ -1,25 +1,14 @@
+Todos = new Mongo.Collection('todos');
+
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
-
-  // set variables that could be used inside the html template
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
-
-  // set event handler
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
+  // template helpers
+  Template.main.helpers({
+    todos: function() {
+      return Todos.find();
     }
   });
 }
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
+  
 }
