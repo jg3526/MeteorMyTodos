018db32ac06fb4b128e7907d6f706091437ee294
Jie Gao <jg3526@users.noreply.github.com>
2015-09-05 12:27:01 -0400

    could add, update, and delete todos, it's working perfectly

--- MeteorMyTodos.js
+++ MeteorMyTodos.js
@@ -4,9 +4,30 @@ if (Meteor.isClient) {
   // template helpers
   Template.main.helpers({
     todos: function() {
-      return Todos.find();
+      return Todos.find({}, {sort: {createdAt: -1}});
     }
   });
+  Template.main.events({
+    "submit .new-todo": function(event) {
+      var text = event.target.text.value;
+      Todos.insert({
+        text: text,
+        createdAt: new Date()
+      });
+      // clear form
+      event.target.text.value = '';
+      return false;
+    },
+    "click .toggle-check": function(event) {
+      Todos.update(this._id, {$set:{checked: !this.checked }});
+    },
+    "click .delete-todo": function(event) {
+      if (confirm("Are you sure?")){
+        Todos.remove(this._id);
+      }
+    }
+  });
+    
 }
 
 if (Meteor.isServer) {
